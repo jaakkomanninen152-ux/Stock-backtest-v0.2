d47b81f70c6de16c01ff4f076884c376c4255d0e
jaakkomanninen152-ux <jaakkomanninen152@gmail.com>
2026-07-11 16:39:25 +0300

    Add files via upload

---
+++ diagnostic_app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Diagnostic test")
+st.write("If you can see this, the base Streamlit platform works fine.")
+st.write("The crash is coming from one of the stock-backtester app's specific dependencies.")
